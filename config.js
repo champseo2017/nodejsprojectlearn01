146d17b2da4c3a485bc4cdc8316609a91d399155
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-08-10 00:38:55 +0700

    update

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    dbOptions:{
+        host:'us-cdbr-iron-east-02.cleardb.net',
+        user:'bd89c04157b71b',
+        password:'ec903f18',
+        database:'heroku_feb8a5e884bb6f2'
+    }
+}
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
+const myConnection = require('express-myconnection')
+const config = require('./config')
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -13,18 +15,7 @@ app.use(function(req, res, next) {
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json({type:'*/*'}))
-
-var con = mysql.createConnection({
-    host:'us-cdbr-iron-east-02.cleardb.net',
-    user:'bd89c04157b71b',
-    password:'ec903f18',
-    database:'heroku_feb8a5e884bb6f2'
-})
-
-con.connect(function(err){
-    if(err) throw err;
-    console.log("Connected!");
-})
+app.use(myConnection(mysql, config.dbOptions, 'pool'))
 
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
