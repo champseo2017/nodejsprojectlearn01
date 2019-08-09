26d354289d792023107c0597811f41c17578eb60
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-08-09 22:56:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-// in sublime
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
-app.get('/', function (req, res) {
- res.send('hello world');
-});
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json({type:'*/*'}))
+
+var con = mysql.createConnection({
+    host:'localhost',
+    user:'hellosay_no01',
+    password:'2RdymJS1a8',
+    database:'hellosay_no01'
+})
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("Connected!");
+})
+
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
 });
