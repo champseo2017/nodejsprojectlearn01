181c052f75cd9e2cde46b0b08620058687b90573
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-08-10 13:34:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
         host:'us-cdbr-iron-east-02.cleardb.net',
         user:'bd89c04157b71b',
         password:'ec903f18',
-        database:'heroku_feb8a5e884bb6f2'
+        database:'heroku_feb8a5e884bb6f2',
+        timezone: 'Asia/Bangkok'
     }
 }
