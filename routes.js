4f4c09446c0eab9b35319b407c6fc42bbf946c9b
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-08-11 14:22:32 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const passportService = require('./service/passport')
 const requireSignin = passport.authenticate('local', {session:false})
 const requireAuth = passport.authenticate('jwt', {session:false})
 const users = require('./controllers/Users')
+const locations = require('./controllers/Locations')
 
 
 module.exports = function (app) {
@@ -18,4 +19,10 @@ module.exports = function (app) {
     app.get('/users/:id', requireAuth, users.findById)
     app.put('/users/:id', requireAuth, users.update)
     app.delete('/users/:id', requireAuth, users.delete)
+
+    app.get('/locations', requireAuth, locations.findAll)
+    app.post('/locations', requireAuth, locations.create)
+    app.get('/locations/:id', requireAuth, locations.findById)
+    app.put('/locations/:id', requireAuth, locations.update)
+    app.delete('/locations/:id', requireAuth, locations.delete)
 }
