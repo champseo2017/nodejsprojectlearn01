773279a83358187bb7d0fb13969fb1b6bb139544
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-08-11 16:22:57 +0700

    update

---
+++ controllers/Works.js
@@ -0,0 +1,97 @@
+const moment = require('moment')
+
+exports.findAll = (req, res, next) =>{
+    req.getConnection((err, connection) => {
+        if(err) return next(err)
+        var repair = req.query.repair
+        sqlWhere = (repair === '1') ? "where work.status < 2":''
+        sql =  `SELECT work.id, work.status,
+        work.doc_date, work.doc_time, work.detail,
+        location.name as location_name FROM work
+        INNER JOIN location ON work.location_id = location.id ${sqlWhere}
+        order by work.doc_date desc, work.doc_time desc`
+        connection.query(sql, (err, results) => {
+            if(err) return next(err)
+            res.send(results)
+        })
+    })
+}
+
+exports.findById = (req, res, next) => {
+    var id = parseInt(req.params.id)
+    req.getConnection((err, connection) => {
+        if(err) return next(err)
+        connection.query("select * from work id=?", [id], (err, results) =>{
+            if(err) return next(err)
+            res.send(results[0])
+        })
+    })
+}
+
+exports.create = (req, res, next) =>{
+    var {body} = req
+    var post = {
+        doc_date: moment().format('YYYY-MM-DD'),
+        doc_time: moment().format('HH:mm'),
+        location_id: body.location_id,
+        detail: body.detail,
+        phone: body.phone,
+        status: 0,
+        user_id: req.user.id
+    }
+    req.getConnection(function(err, connection){
+        if(err) return next(err)
+        connection.query("insert into work set ?", post, (err, results) => {
+            if(err) return next(err)
+            res.send(results)
+        })
+    })
+}
+
+exports.update = (req, res, next) => {
+    var id = parseInt(req.params.id)
+    var {body} = req
+    var post = {
+        location_id: body.location_id,
+        detail: body.detail,
+        phone: body.phone
+    }
+    req.getConnection((err, connection) => {
+        if(err) next(err)
+        connection.query("update work set ? where id=?", [post, id], (err, results) => {
+            if(err) return next(err)
+            res.send(results)
+        })
+    })
+}
+
+exports.updateRepair = (req, res, next) => {
+    var id = parseInt(req.params.id)
+    var {body} = req
+    var post = {
+        status: body.status,
+        status_date: moment().format('YYYY-MM-DD'),
+        status_time: moment().format('HH:mm'),
+        work_detail: body.work_detail,
+        work_user_id: req.user.id
+    }
+
+    req.getConnection((err, connection) => {
+        if(err) return next(err)
+        connection.query("update work set ? where id=?", [post, id], (err, results) =>{
+            if(err) return next(err)
+            res.send(results)
+        })
+    })
+}
+
+exports.delete = (req, res, next) =>{
+    var id = parseInt(req.params.id)
+    req.getConnection((err, connection) => {
+        if(err) return next(err)
+        connection.query("delete from work where id=?", [id], (err, results) =>{
+            if(err) return next(err)
+            res.send(results)
+        })
+    })
+}
--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const requireSignin = passport.authenticate('local', {session:false})
 const requireAuth = passport.authenticate('jwt', {session:false})
 const users = require('./controllers/Users')
 const locations = require('./controllers/Locations')
+const works = require('./controllers/Works')
 
 
 module.exports = function (app) {
@@ -25,4 +26,12 @@ module.exports = function (app) {
     app.get('/locations/:id', requireAuth, locations.findById)
     app.put('/locations/:id', requireAuth, locations.update)
     app.delete('/locations/:id', requireAuth, locations.delete)
+
+    app.get('/works', requireAuth, works.findAll)
+    app.post('/works', requireAuth, works.create)
+    app.get('/works/:id', requireAuth, works.findById)
+    app.put('/works/:id', requireAuth, works.update)
+    app.put('/works/:id/repair', requireAuth, works.updateRepair)
+    app.delete('/works/:id', requireAuth, works.delete)
+    
 }
