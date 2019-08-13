a407e73388f4b88c9234594c4c4b2f079ace8f45
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-08-13 15:41:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,9 @@ const config = require('./config')
 const routes = require('./routes')
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
-    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // next();
 });
 
 app.use(bodyParser.urlencoded({extended:true}))
