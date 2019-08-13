6bec249356937dfed8f5b5375ef34a144e89260d
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-08-13 17:38:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ const myConnection = require('express-myconnection')
 const config = require('./config')
 const routes = require('./routes')
 
-
 app.use(function(req, res, next) {
 
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+    // res.header("Access-Control-Allow-Origin", '*');
+    // res.header("Access-Control-Allow-Credentials", true);
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    // res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
+
 });
 
 app.use(bodyParser.urlencoded({extended:true}))
