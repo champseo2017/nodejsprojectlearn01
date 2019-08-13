fbaa5e40e9b8f69103d7617184bae396995382a2
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-08-13 16:31:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ const routes = require('./routes')
 
 app.use(function(req, res, next) {
 
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    // res.header("Access-Control-Allow-Origin", '*');
+    // res.header("Access-Control-Allow-Credentials", true);
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    // res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
    
     // res.header("Access-Control-Allow-Origin", "*");
     // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
