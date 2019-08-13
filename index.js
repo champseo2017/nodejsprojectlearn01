f690eafe55c9c91fcd96dd56b4de38f5acc0b4c7
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-08-13 17:21:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ const mysql = require('mysql')
 const myConnection = require('express-myconnection')
 const config = require('./config')
 const routes = require('./routes')
+import cors from 'cors';
 
-app.use(function(req, res, next) {
+// app.use(function(req, res, next) {
 
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
+app.use(cors()); // <---- use cors middleware
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json({type:'*/*'}))
