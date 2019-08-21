62b2f44a047c9a4feac63720c38c613f60d0116c
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-08-21 16:54:18 +0700

    Update Works.js

--- controllers/Works.js
+++ controllers/Works.js
@@ -1,17 +1,18 @@
 const moment = require('moment')
 
-exports.findAll = (req, res, next) =>{
+exports.findAll = (req, res, next) => {
     req.getConnection((err, connection) => {
-        if(err) return next(err)
+        if (err) return next(err)
+
         var repair = req.query.repair
-        sqlWhere = (repair === '1') ? "where work.status < 2":''
-        sql =  `SELECT work.id, work.status,
+        sqlWhere = (repair === '1') ? " where work.status < 2" : ''
+        sql = `SELECT work.id, work.status,
         work.doc_date, work.doc_time, work.detail,
-        location.name as location_name FROM work
-        INNER JOIN location ON work.location_id = location.id ${sqlWhere}
+        location.name as location_name FROM work 
+        INNER JOIN location ON work.location_id = location.id ${sqlWhere} 
         order by work.doc_date desc, work.doc_time desc`
         connection.query(sql, (err, results) => {
-            if(err) return next(err)
+            if (err) return next(err)
             res.send(results)
         })
     })
@@ -20,16 +21,16 @@ exports.findAll = (req, res, next) =>{
 exports.findById = (req, res, next) => {
     var id = parseInt(req.params.id)
     req.getConnection((err, connection) => {
-        if(err) return next(err)
-        connection.query("select * from work id=?", [id], (err, results) =>{
-            if(err) return next(err)
+        if (err) return next(err)
+        connection.query("select * from work where id=?", [id], (err, results) => {
+            if (err) return next(err)
             res.send(results[0])
         })
     })
 }
 
-exports.create = (req, res, next) =>{
-    var {body} = req
+exports.create = (req, res, next) => {
+    var { body } = req
     var post = {
         doc_date: moment().format('YYYY-MM-DD'),
         doc_time: moment().format('HH:mm'),
@@ -37,12 +38,12 @@ exports.create = (req, res, next) =>{
         detail: body.detail,
         phone: body.phone,
         status: 0,
-        user_id: req.user.id
+        user_id: req.user.id,
     }
-    req.getConnection(function(err, connection){
-        if(err) return next(err)
-        connection.query("insert into work set ?", post, (err, results) => {
-            if(err) return next(err)
+
+    req.getConnection(function (err, connection) {
+        connection.query("insert into work set ? ", post, (err, results) => {
+            if (err) return next(err)
             res.send(results)
         })
     })
@@ -50,16 +51,16 @@ exports.create = (req, res, next) =>{
 
 exports.update = (req, res, next) => {
     var id = parseInt(req.params.id)
-    var {body} = req
+    var { body } = req
     var post = {
         location_id: body.location_id,
         detail: body.detail,
-        phone: body.phone
+        phone: body.phone,
     }
-    req.getConnection((err, connection) => {
-        if(err) next(err)
-        connection.query("update work set ? where id=?", [post, id], (err, results) => {
-            if(err) return next(err)
+
+    req.getConnection(function (err, connection) {
+        connection.query("update work set ? where id=?", [post, id], function (err, results) {
+            if (err) return next(err)
             res.send(results)
         })
     })
@@ -67,30 +68,29 @@ exports.update = (req, res, next) => {
 
 exports.updateRepair = (req, res, next) => {
     var id = parseInt(req.params.id)
-    var {body} = req
+    var { body } = req
     var post = {
         status: body.status,
         status_date: moment().format('YYYY-MM-DD'),
         status_time: moment().format('HH:mm'),
         work_detail: body.work_detail,
-        work_user_id: req.user.id
+        work_user_id: req.user.id,
     }
 
-    req.getConnection((err, connection) => {
-        if(err) return next(err)
-        connection.query("update work set ? where id=?", [post, id], (err, results) =>{
-            if(err) return next(err)
+    req.getConnection(function (err, connection) {
+        connection.query("update work set ? where id=?", [post, id], function (err, results) {
+            if (err) return next(err)
             res.send(results)
         })
     })
 }
 
-exports.delete = (req, res, next) =>{
+exports.delete = (req, res, next) => {
     var id = parseInt(req.params.id)
     req.getConnection((err, connection) => {
-        if(err) return next(err)
-        connection.query("delete from work where id=?", [id], (err, results) =>{
-            if(err) return next(err)
+        if (err) return next(err)
+        connection.query("delete from work where id=?", [id], (err, results) => {
+            if (err) return next(err)
             res.send(results)
         })
     })
